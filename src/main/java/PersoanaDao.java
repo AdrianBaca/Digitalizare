206e25c87b85b8af37ccab1a4edb20e3d3617c6d
AdrianBaca <bacaadi@yahoo.com>
2023-03-22 23:47:24 +0200

    Task 1

---
+++ src/main/java/PersoanaDao.java
@@ -0,0 +1,7 @@
+import java.util.List;
+public interface PersoanaDao {
+    public void adaugare(Persoana p);
+    public void modificare(Persoana p);
+    public void stergere(Persoana p);
+    public void  interogare();
+}
---
+++ src/main/java/PersoanaDaoImpl.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+public class PersoanaDaoImpl implements PersoanaDao{
+    List<Persoana> persoane;
+    public PersoanaDaoImpl(){
+
+    }
+
+    @Override
+    public void adaugare(Persoana p){
+
+    }
+    @Override
+    public void stergere(Persoana p){
+
+
+    }
+
+    @Override
+    public void modificare(Persoana p) {
+
+    }
+
+    @Override
+    public void interogare() {
+
+    }
+}
